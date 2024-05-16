91e517b2f1e41603be4342e48c8c20389528a4e6
Baptiste <brument.bcb@gmail.com>
2024-05-16 14:48:54 +0200

    run_3steps update

--- run_3steps.sh
+++ run_3steps.sh
@@ -10,21 +10,16 @@ fi
 case="$1"
 
 # Execute commands with the provided folder path
-./build/testbed --scene "${case}/NeuS2/NeuS2_l60/" \
+./build/testbed --scene "${case}/NeuS2_no_light" \
     --maxiter 5000 --save-mesh --save-snapshot --mask-weight 1.0 --no-gui
+mv "${case}/snapshot_5000.msgpack" "${case}/NeuS2_no_light/snapshot_5000.msgpack"
 
-./build/testbed --scene "${case}/NeuS2/NeuS2_l60/" \
+./build/testbed --scene "${case}/NeuS2_no_light" \
     --maxiter 15000 --save-mesh --save-snapshot --mask-weight 0.3 \
-    --snapshot "${case}/NeuS2/NeuS2_l60/snapshot_5000.msgpack" --no-gui
+    --snapshot "${case}/NeuS2_no_light/snapshot_5000.msgpack" --no-gui
+mv "${case}/snapshot_15000.msgpack" "${case}/NeuS2_no_light/snapshot_15000.msgpack"
 
-./build/testbed --opti-lights --scene "${case}/NeuS2/NeuS2_lopti/" \
+./build/testbed --opti-lights --scene "${case}/NeuS2_no_light" \
     --maxiter 25000 --save-mesh --save-snapshot --mask-weight 0.3 \
-    --snapshot "${case}/NeuS2/NeuS2_l60/snapshot_15000.msgpack" --no-gui
-
-# Create result directory if not exists
-mkdir -p "${case}/result/"
-
-# Copy files to the result directory
-cp "${case}/NeuS2/NeuS2_l60/mesh_5000_.obj" "${case}/result/mesh_5000.obj"
-cp "${case}/NeuS2/NeuS2_l60/mesh_15000_.obj" "${case}/result/mesh_15000.obj"
-cp "${case}/NeuS2/NeuS2_lopti/mesh_25000_.obj" "${case}/result/mesh_25000.obj"
+    --snapshot "${case}/NeuS2_no_light/snapshot_15000.msgpack" --no-gui
+mv "${case}/snapshot_25000.msgpack" "${case}/NeuS2_no_light/snapshot_25000.msgpack"
